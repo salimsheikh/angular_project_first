e6daa65b459133bfe97f04e91efeb3b0b37f0703
Salim Shaikh <salimsheikh4u2000@gmail.com>
2023-05-30 15:31:10 +0530

    Auth Guard Completed

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { SignUpDataType } from '../data-type';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
+  isSellerLoggedIn = new BehaviorSubject<Boolean>(false);
+  constructor(private http: HttpClient, private router: Router) { }
+  userSignUpService(data: SignUpDataType) {
+    let results = this.http.post("http://localhost:3000/seller", data, { observe: 'response' }).subscribe((results) => {
+      this.isSellerLoggedIn.next(true);
+      localStorage.setItem('seller', JSON.stringify(results.body));
+      this.router.navigate(['seller-home']);
+    });
+  }
 
-  constructor(private http:HttpClient) { }
-  /*
-      data type: any|object
-   */
-  userSignUpService(data:SignUpDataType){
-    return this.http.post("http://localhost:3000/seller",data);
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
+      this.isSellerLoggedIn.next(true);
+      this.router.navigate(['seller-home']);
+    } else {
+      //this.router.navigate(['seller-auth']);
+    }
   }
 }
